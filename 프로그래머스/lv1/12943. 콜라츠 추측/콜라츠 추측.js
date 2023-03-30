b3e4263efe6347940f64dbf2cca4f1a36b92b5f3
Choi Yeseul <117844745+dd-stack@users.noreply.github.com>
2023-03-30 23:44:42 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.12 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12943. 콜라츠 추측/콜라츠 추측.js
@@ -0,0 +1,20 @@
+let result = 0;
+function solution(num) {
+    if(num === 1){
+        return result;
+    }
+    if(result >= 500){
+            return -1
+    }
+    let n = 0;
+    if(num % 2 === 0){
+        n = num / 2;
+        result += 1 
+        return solution(n);
+    }
+    if(num % 2 === 1){
+        n = num * 3 + 1;
+        result += 1 
+        return solution(n);
+    }
+}
