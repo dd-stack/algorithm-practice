4c650c9d57ca9a6b724a0b7a4ef440e5b21cdc53
Yeseul Choi <117844745+dd-stack@users.noreply.github.com>
2023-06-26 13:56:09 +0900

    [unrated] Title: 마지막 두 원소, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181927. 마지막 두 원소/마지막 두 원소.js
@@ -0,0 +1,11 @@
+function solution(num_list) {
+    const answer = [...num_list]
+    const lastIdx = num_list.length - 1;
+    if(num_list[lastIdx] > num_list[lastIdx - 1]){
+        answer.push(num_list[lastIdx] - num_list[lastIdx - 1]);
+    } 
+    if(num_list[lastIdx] <= num_list[lastIdx - 1]){
+       answer.push(num_list[lastIdx] * 2);
+       }
+    return answer;
+}
