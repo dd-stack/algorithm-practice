66f2982e015c0798d79bc880df7b3f9537a4cace
Yeseul Choi <117844745+dd-stack@users.noreply.github.com>
2023-09-04 00:47:10 +0900

    [level 1] Title: K번째수, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/42748. K번째수/K번째수.js
@@ -0,0 +1,12 @@
+function solution(array, commands) {
+    const answer = [];
+    for(let i = 0; i < commands.length; i++){
+        const firstIdx = commands[i][0] - 1;
+        const lastIdx = commands[i][1];
+        const arr = array.slice(firstIdx, lastIdx);
+        const sortArr = arr.sort((x, y) => x - y);
+        
+        answer.push(sortArr[commands[i][2] - 1])
+    }
+    return answer
+}
