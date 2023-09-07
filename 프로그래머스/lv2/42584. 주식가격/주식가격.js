54069df7749d09554c3166c9ea98b978adb97f64
Yeseul Choi <117844745+dd-stack@users.noreply.github.com>
2023-09-07 17:23:22 +0900

    [level 2] Title: 주식가격, Time: 6.02 ms, Memory: 42.1 MB -BaekjoonHub

--- 프로그래머스/lv2/42584. 주식가격/주식가격.js
+++ 프로그래머스/lv2/42584. 주식가격/주식가격.js
@@ -20,52 +20,52 @@ function solution(prices) {
     
     // 스택으로 풀어보기 -> 시간복잡도 O(n)
     
-    // prices와 동일한 길이 & 0으로 채워진 배열을 생성
-    const answer = new Array(prices.length).fill(0);
-    // 스택으로 사용할 배열을 생성 (인덱스를 저장할 예정)
-    const stack = [];
+//     // prices와 동일한 길이 & 0으로 채워진 배열을 생성
+//     const answer = new Array(prices.length).fill(0);
+//     // 스택으로 사용할 배열을 생성 (인덱스를 저장할 예정)
+//     const stack = [];
     
-    // 모든 prices를 돌면서,
-    for (let i = 0; i < prices.length; i++) {
-        // 스택이 비어있지 않고, 가격이 떨어졌을 경우
-        // prices[stack[stack.length - 1]] -> 이전 시점에서 스택에 저장된 가격
-        while (stack.length > 0 && prices[i] < prices[stack[stack.length - 1]]) {
-            // 스택에서 이전 시점의 가격(인덱스)을 꺼내서 (스택에서는 제거됨)
-            const top = stack.pop();
-            // 가격이 떨어지지 않은 시간을 계산해 answer에 넣는다.
-            answer[top] = i - top;
-        }
-        // 스택에 인덱스를 넣는다. 
-        stack.push(i);
-    }
+//     // 모든 prices를 돌면서,
+//     for (let i = 0; i < prices.length; i++) {
+//         // 스택이 비어있지 않고, 가격이 떨어졌을 경우
+//         // prices[stack[stack.length - 1]] -> 이전 시점에서 스택에 저장된 가격
+//         while (stack.length > 0 && prices[i] < prices[stack[stack.length - 1]]) {
+//             // 스택에서 이전 시점의 가격(인덱스)을 꺼내서 (스택에서는 제거됨)
+//             const top = stack.pop();
+//             // 가격이 떨어지지 않은 시간을 계산해 answer에 넣는다.
+//             answer[top] = i - top;
+//         }
+//         // 스택에 인덱스를 넣는다. 
+//         stack.push(i);
+//     }
     
-    // 스택에 남아 있는 인덱스들을 처리한다.
-    while (stack.length > 0) {
-        const top = stack.pop();
-        answer[top] = prices.length - 1 - top;
-    }
+//     // 스택에 남아 있는 인덱스들을 처리한다.
+//     while (stack.length > 0) {
+//         const top = stack.pop();
+//         answer[top] = prices.length - 1 - top;
+//     }
     
-    return answer;
+//     return answer;
     
     
     
     // 큐로도 풀어보기
     
-    // const answer = new Array(prices.length).fill(0);
-    // const queue = [];
-    // 
-    // for(let i = 0; i < prices.length; i++){
-    //     while(queue.length > 0 && prices[i] < prices[queue[0]]) {
-    //         const bottom = queue.shift();
-    //         answer[bottom] = i - bottom;
-    //     }
-    //     queue.unshift(i);
-    // }
-    // 
-    // while(queue.length > 0) {
-    //     const bottom = queue.shift();
-    //     answer[bottom] = prices.length - 1 - bottom;
-    // }
-    // 
-    // return answer;
+    const answer = new Array(prices.length).fill(0);
+    const queue = [];
+    
+    for(let i = 0; i < prices.length; i++){
+        while(queue.length > 0 && prices[i] < prices[queue[0]]) {
+            const bottom = queue.shift();
+            answer[bottom] = i - bottom;
+        }
+        queue.unshift(i);
+    }
+    
+    while(queue.length > 0) {
+        const bottom = queue.shift();
+        answer[bottom] = prices.length - 1 - bottom;
+    }
+    
+    return answer;
 }
